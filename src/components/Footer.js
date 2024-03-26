bc656c3df6d743b30c18518d53989717cdfcc850
Sammy Misbah <sammy.misbah.dev@gmail.com>
2024-03-26 16:43:54 +0100

    responsive start

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,13 @@ const Footer = () => {
   return (
     <div className="bg-beachliga_grey p-12">
       <div className="max-w-[1200px] mx-auto flex justify-between items-center">
-        <img src="./assets/linkedin.png" />
+        <a
+          href="https://www.linkedin.com/company/revel-cool/about/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="./assets/linkedin.png" />
+        </a>
         <div className="flex mr-[10px] text-white space-x-12 text-md">
           <div>Términos y Condiciones</div>
           <div>Contacta con nosotros</div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="bg-beachliga_grey h-[calc(100vh-65px)]">
-      <div className="flex justify-between max-w-[1200px] mx-auto">
-        <div className="flex flex-col justify-center m-6 py-6">
-          <h1 className="text-6xl text-beachliga_blue font-[400] mb-6">
+    <div className="bg-beachliga_grey lg:h-[calc(100vh-65px)]">
+      <div className="flex flex-col max-w-[1200px] mx-auto lg:flex-row lg:justify-between">
+        <div className="flex flex-col justify-center items-center lg:items-start lg:h-[calc(100vh-65px)] my-6 mx-12 py-6">
+          <h1 className="text-center text-6xl text-beachliga_blue font-[400] mb-6 lg:text-start">
             Descubre una
             <br /> nueva forma de
             <br /> hacer <span className="text-beachliga_yellow">deporte</span>
@@ -38,41 +38,43 @@ const Home = () => {
             />
           </div>
         </div>
-        <div className="flex flex-col py-12 relative h-[calc(100vh-56px)]">
-          <img
-            src="./assets/training_screenshot.png"
-            className="min-w-[320px] rounded-[35px] absolute right-[70px] top-[50px] shadow-2xl border-[5px] border-beachliga_neutral"
-            alt="training_screenshot"
-          />
-          <img
-            src="./assets/login_screenshot.png"
-            className="min-w-[320px] rounded-[35px] absolute right-[140px] top-[130px] shadow-2xl border-[5px] border-beachliga_neutral"
-            alt="matches_screenshot"
-          />
-          <div className="absolute top-[560px] right-[390px] bg-white w-[200px] text-sm px-4 py-3 rounded-lg">
-            <div className="flex space-x-1 mb-2">
-              <img src="./assets/star.png" alt="star" />
-              <img src="./assets/star.png" alt="star" />
-              <img src="./assets/star.png" alt="star" />
-              <img src="./assets/star.png" alt="star" />
-              <img src="./assets/star.png" alt="star" />
+        <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center justify-center py-12 relative h-[calc(100vh+250px)]">
+            <img
+              src="./assets/training_screenshot.png"
+              className="hidden lg:block min-w-[320px] rounded-[35px] lg:absolute right-[100px] lg:right-[70px] top-[50px] shadow-2xl border-[5px] border-beachliga_neutral"
+              alt="training_screenshot"
+            />
+            <img
+              src="./assets/login_screenshot.png"
+              className="min-w-[320px] rounded-[35px] lg:absolute right-[170px] lg:right-[140px] top-[130px] shadow-2xl border-[5px] border-beachliga_neutral"
+              alt="matches_screenshot"
+            />
+            <div className="hidden xl:block absolute top-[560px] right-[390px] bg-white w-[200px] text-sm px-4 py-3 rounded-lg">
+              <div className="flex space-x-1 mb-2">
+                <img src="./assets/star.png" alt="star" />
+                <img src="./assets/star.png" alt="star" />
+                <img src="./assets/star.png" alt="star" />
+                <img src="./assets/star.png" alt="star" />
+                <img src="./assets/star.png" alt="star" />
+              </div>
+              <span className="font-semibold  text-beachliga_neutral">
+                Puedo crear un torneo en menos de 1 minuto y ver fácilmente
+                quién se ha inscrito
+              </span>
             </div>
-            <span className="font-semibold  text-beachliga_neutral">
-              Puedo crear un torneo en menos de 1 minuto y ver fácilmente quién
-              se ha inscrito
-            </span>
-          </div>
-          <div className="absolute top-[210px] right-[-90px] bg-white w-[200px] text-sm px-4 py-3 rounded-lg">
-            <div className="flex space-x-1 mb-2">
-              <img src="./assets/star.png" alt="star" />
-              <img src="./assets/star.png" alt="star" />
-              <img src="./assets/star.png" alt="star" />
-              <img src="./assets/star.png" alt="star" />
-              <img src="./assets/star.png" alt="star" />
+            <div className="hidden xl:block absolute top-[210px] right-[-90px] bg-white w-[200px] text-sm px-4 py-3 rounded-lg">
+              <div className="flex space-x-1 mb-2">
+                <img src="./assets/star.png" alt="star" />
+                <img src="./assets/star.png" alt="star" />
+                <img src="./assets/star.png" alt="star" />
+                <img src="./assets/star.png" alt="star" />
+                <img src="./assets/star.png" alt="star" />
+              </div>
+              <span className="font-semibold  text-beachliga_neutral">
+                Una app genial para encontrar con quién entrenar
+              </span>
             </div>
-            <span className="font-semibold  text-beachliga_neutral">
-              Una app genial para encontrar con quién entrenar
-            </span>
           </div>
         </div>
       </div>
