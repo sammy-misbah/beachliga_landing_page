e0bd9563890b46764c8e87afce0cf3e692e897b8
Sammy Misbah <sammy.misbah.dev@gmail.com>
2024-04-11 12:30:05 +0200

    b

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import "./App.css";
-
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Terms from "./pages/Terms";
@@ -7,7 +5,7 @@ import Whatsapp from "./utils/Whatsapp";
 
 function App() {
   return (
-    <div>
+    <div className="max-w-screen">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/terms" element={<Terms />} />
