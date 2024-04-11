832acd10aa85979514996e2167b6570068c2e926
Sammy Misbah <sammy.misbah.dev@gmail.com>
2024-04-11 12:34:12 +0200

    c

--- src/utils/Whatsapp.js
+++ src/utils/Whatsapp.js
@@ -3,12 +3,16 @@ import React from "react";
 const Whatsapp = () => {
   return (
     <a
-      className="fixed bottom-12 right-12 h-16 w-16 z-10"
+      className=""
       href="https://wa.me/34600256862"
       target="_blank"
       rel="noreferrer"
     >
-      <img alt="whatsapp" src="./assets/whatsapp.webp" className="z-10" />
+      <img
+        alt="whatsapp"
+        src="./assets/whatsapp.webp"
+        className="z-10 fixed bottom-12 right-12 h-16 w-16"
+      />
     </a>
   );
 };
